3d701cf72e3f1dfe3ad4661d7804f88b14745319
rubenmoya <rmoyarodriguez@gmail.com>
2019-11-11 08:26:09 +0100

    fix: ord should extend from setoid

--- src/ord.ts
+++ src/ord.ts
@@ -1,6 +1,8 @@
+import { Setoid } from './setoid';
+
 // https://github.com/fantasyland/fantasy-land/blob/master/README.md#ord
 
-export interface Ord<A> {
+export interface Ord<A> extends Setoid<A> {
   // fantasy-land/lte :: Ord a => a ~> a -> Boolean
   lte(a: A): boolean;
 }
