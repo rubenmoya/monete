e928cfc9c6759e58fce21936e9a4a4fbc7e7141b
Daniel Ramos <danielramosacosta@hotmail.com>
2020-07-19 14:56:03 +0100

    Stuff

--- __test__/identity.spec.ts
+++ __test__/identity.spec.ts
@@ -9,39 +9,19 @@ describe('.of', () => {
   });
 });
 
-describe('.toString', () => {
-  it('returns the type and value', () => {
-    const one = Identity.of(1);
-
-    expect(one.toString()).toEqual('Identity(1)');
-  });
-});
-
-describe('.map', () => {
-  it('returns a new Identity with the mapped function applied', () => {
-    const one = Identity.of(1);
-    const two = one.map(a => a + 1);
-
-    expect(two).toBeInstanceOf(Identity);
-    expect(two).toEqual(Identity.of(2));
-  });
-});
-
-describe('.flatMap', () => {
-  it('returns a new value with the mapped function applied', () => {
-    const one = Identity.of(1);
-    const two = one.flatMap(a => Identity.of(a + 1));
-
-    expect(two).toBeInstanceOf(Identity);
-    expect(two).toEqual(Identity.of(2));
-  });
-});
-
 describe('.identity', () => {
   it('returns the inner value of a nested identity monad', () => {
     const MONAD_INNER_NUMBER = 1;
-    const one = Identity.of(Identity.of(Identity.of(MONAD_INNER_NUMBER)));
-    const result: number = one.join();
+    const one = Identity.of(
+      Identity.of(
+        Identity.of(
+          Identity.of(
+            Identity.of(Identity.of(Identity.of(MONAD_INNER_NUMBER))),
+          ),
+        ),
+      ),
+    );
+    const result = one.join(); // result es de tipo number
 
     expect(result).toEqual(MONAD_INNER_NUMBER);
   });
--- src/identity.ts
+++ src/identity.ts
@@ -1,23 +1,23 @@
 export class Identity<T> {
-  private readonly value: T;
+  private readonly value: T | Identity<T>;
 
-  constructor(value: T) {
+  constructor(value: Identity<T>);
+  constructor(value: T);
+
+  constructor(value: T | Identity<T>) {
     this.value = value;
   }
 
-  static of<T>(value: T): Identity<T> {
+  static of<T>(value: Identity<T>): Identity<T>;
+  static of<T>(value: T): Identity<T>;
+  static of<T>(value: T | Identity<T>) {
     return new Identity(value);
   }
 
-  map<V>(fn: (val: T) => V): Identity<V> {
-    return Identity.of(fn(this.value));
-  }
-
-  flatMap<V>(fn: (val: T) => Identity<V>): Identity<V> {
-    return fn(this.value);
-  }
-
-  toString(): string {
-    return `Identity(${this.value})`;
+  join(): T {
+    if (this.value instanceof Identity) {
+      return this.value.join();
+    }
+    return this.value;
   }
 }
